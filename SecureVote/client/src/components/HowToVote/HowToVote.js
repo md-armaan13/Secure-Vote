fba4d49f502bd3f3f01482dc397fbd977e06a840
SION CHOWDHARY <94757639+sion-chowdhary@users.noreply.github.com>
2023-01-07 06:10:06 +0530

    added loader

--- SecureVote/client/src/components/HowToVote/HowToVote.js
+++ SecureVote/client/src/components/HowToVote/HowToVote.js
@@ -1,81 +1,103 @@
-import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import {
-	FormColumn,
-	FormWrapper,
-	FormInput,
-	FormSection,
-	FormRow,
-	FormLabel,
-	FormInputRow,
-	FormMessage,
-	FormButton,
-	FormTitle,
-} from './HowToVoteStyle';
-import { Link } from 'react-router-dom';
-import './button68.css';
+  FormColumn,
+  FormWrapper,
+  FormInput,
+  FormSection,
+  FormRow,
+  FormLabel,
+  FormInputRow,
+  FormMessage,
+  FormButton,
+  FormTitle,
+} from "./HowToVoteStyle";
+import { Link } from "react-router-dom";
+import "./button68.css";
 
-import { Container } from '../../globalStyles';
+import { Container } from "../../globalStyles";
 // import validateForm from './validateForm';
-import Particle from '../Particle';
+import Particle from "../Particle";
 
 const HowToVote = () => {
-	const [name, setName] = useState('');
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [confirmPass, setConfirmPass] = useState('');
-	const [error, setError] = useState(null);
-	const [success, setSuccess] = useState(null);
-	let history = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  let history = useNavigate();
 
-	
-	const messageVariants = {
-		hidden: { y: 30, opacity: 0 },
-		animate: { y: 0, opacity: 1, transition: { delay: 0.2, duration: 0.4 } },
-	};
+  const messageVariants = {
+    hidden: { y: 30, opacity: 0 },
+    animate: { y: 0, opacity: 1, transition: { delay: 0.2, duration: 0.4 } },
+  };
 
-	// const formData = [
-	// 	{ label: 'Name', value: name, onChange: (e) => setName(e.target.value), type: 'text' },
-	// 	{ label: 'Email', value: email, onChange: (e) => setEmail(e.target.value), type: 'email' },
-	// 	{
-	// 		label: 'Password',
-	// 		value: password,
-	// 		onChange: (e) => setPassword(e.target.value),
-	// 		type: 'password',
-	// 	},
-	// 	{
-	// 		label: 'Confirm Password',
-	// 		value: confirmPass,
-	// 		onChange: (e) => setConfirmPass(e.target.value),
-	// 		type: 'password',
-	// 	},
-	// ];
+  // const formData = [
+  // 	{ label: 'Name', value: name, onChange: (e) => setName(e.target.value), type: 'text' },
+  // 	{ label: 'Email', value: email, onChange: (e) => setEmail(e.target.value), type: 'email' },
+  // 	{
+  // 		label: 'Password',
+  // 		value: password,
+  // 		onChange: (e) => setPassword(e.target.value),
+  // 		type: 'password',
+  // 	},
+  // 	{
+  // 		label: 'Confirm Password',
+  // 		value: confirmPass,
+  // 		onChange: (e) => setConfirmPass(e.target.value),
+  // 		type: 'password',
+  // 	},
+  // ];
 
-	return (
-		
-		<FormSection method ="POST">
-			<Particle/>
-			<Container>
-				<FormRow>
-					<FormColumn small>
-						<FormTitle>How To Vote</FormTitle>
-						<FormWrapper >
-                           <p>
-                           <ul>  To cast your valuable vote you have to follow the below listed steps:<br/><br/><br/>
-<li/> On the landing page click on VOTE HERE <br/><br/>
-<li/>
-A window appears that asks for your voter id verification appears. Upload the photos of your voter id card so that verification can be done <br/><br/>
-<li/>
-After completing the voter id verification, click on PROCEED. A new window for Aadhar card verification appears, upload the photos of your aadhar card for verification<br/><br/>
-<li/>
-When the aadhar card is verified, click on PROCEED after which a window will appear for face detection. Allow your camera and click a photograph of yours.<br/><br/>
-<li/>
-The photograph will be matched with the existing database and after clicking PROCEED you would be allowed to vote
-A screen showing the information of the parties will appear, on which you can cast your vote and claim your NFT.
-<br/><br/>
-</ul>
-                            </p>
-							{/* {formData.map((el, index) => (
+  return (
+    <FormSection method="POST">
+      <Particle />
+      <Container>
+        <FormRow>
+          <FormColumn small>
+            <FormTitle>How To Vote</FormTitle>
+            <FormWrapper>
+              <p>
+                <ul>
+                  {" "}
+                  To cast your valuable vote you have to follow the below listed
+                  steps:
+                  <br />
+                  <br />
+                  <br />
+                  <li > On the landing page click on VOTE HERE </li >
+                  <br />
+                  <li >
+                  A window appears that asks for your voter id verification
+                  appears. Upload the photos of your voter id card so that
+                  verification can be done </li>
+				  <br />
+                  <br />
+                  <li >
+                  After completing the voter id verification, click on PROCEED.
+                  A new window for Aadhar card verification appears, upload the
+                  photos of your aadhar card for verification </li>
+                  <br />
+                  <br />
+                  <li >
+                  When the aadhar card is verified, click on PROCEED after which
+                  a window will appear for face detection. Allow your camera and
+                  click a photograph of yours.
+				  </li>
+                  <br />
+                  <br />
+                  <li >
+                  The photograph will be matched with the existing database and
+                  after clicking PROCEED you would be allowed to vote A screen
+                  showing the information of the parties will appear, on which
+                  you can cast your vote and claim your NFT.
+				  </li>
+                  <br />
+                  <br />
+                </ul>
+              </p>
+              {/* {formData.map((el, index) => (
 								<FormInputRow key={index}>
 									<FormLabel>{el.label}</FormLabel>
 									<FormInput
@@ -87,42 +109,34 @@ A screen showing the information of the parties will appear, on which you can ca
 								</FormInputRow>
 							))} */}
 
-                            <Link to="/voterId">
-
-                            <button class="btn-68">Next</button>
-
-                             
-
-                            
-						
-						    </Link>
-
-						</FormWrapper>
-						{error && (
-							<FormMessage
-								variants={messageVariants}
-								initial="hidden"
-								animate="animate"
-								error
-							>
-								{error}
-							</FormMessage>
-						)}
-						{success && (
-							<FormMessage
-								variants={messageVariants}
-								initial="hidden"
-								animate="animate"
-							>
-								{success}
-							</FormMessage>
-						)}
-					</FormColumn>
-				</FormRow>
-			</Container>
-		</FormSection>
-		
-	);
+              <Link to="/voterId">
+                <button class="btn-17">Next</button>
+              </Link>
+            </FormWrapper>
+            {error && (
+              <FormMessage
+                variants={messageVariants}
+                initial="hidden"
+                animate="animate"
+                error
+              >
+                {error}
+              </FormMessage>
+            )}
+            {success && (
+              <FormMessage
+                variants={messageVariants}
+                initial="hidden"
+                animate="animate"
+              >
+                {success}
+              </FormMessage>
+            )}
+          </FormColumn>
+        </FormRow>
+      </Container>
+    </FormSection>
+  );
 };
 
 export default HowToVote;
